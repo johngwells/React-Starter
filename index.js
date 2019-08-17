79ecd88aaa85fe6372fcb897a35b0cb058cb56c9
John Wells <molekewl2017@gmail.com>
2019-08-16 23:23:45 -0700

    add bundler

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Hi')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = <div>React element with JSX</div>
+
+// const element = React.createElement('div', null, 'React element!');
+
+ReactDOM.render(element, document.getElementById('root'));
